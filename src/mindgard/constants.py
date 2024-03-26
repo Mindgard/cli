5e3e18e31dbc4a88d0d653c02da78d22a5bad66e
Danny Hunt <danny.hunt@mindgard.ai>
2024-03-26 16:29:22 +0000

    Release 0.9.0

--- src/mindgard/constants.py
+++ src/mindgard/constants.py
@@ -1,5 +1,5 @@
 
-VERSION: str = "0.8.2"
+VERSION: str = "0.9.0"
 REPOSITORY_URL: str = f"https://pypi.org/pypi/mindgard/json"
 AUTH0_DOMAIN= "login.sandbox.mindgard.ai"
 AUTH0_CLIENT_ID= "U0OT7yZLJ4GEyabar11BENeQduu4MaNO"
