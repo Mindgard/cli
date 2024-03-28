af52613e0e756bada33495e043ab196d3d4d3d01
Danny Hunt <danny.hunt@mindgard.ai>
2024-03-28 16:08:22 +0000

    Release 0.13.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="mindgard",  # Required
-    version="0.12.0",
+    version="0.13.0",
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
--- src/mindgard/constants.py
+++ src/mindgard/constants.py
@@ -1,5 +1,5 @@
 
-VERSION: str = "0.12.0"
+VERSION: str = "0.13.0"
 REPOSITORY_URL: str = f"https://pypi.org/pypi/mindgard/json"
 AUTH0_DOMAIN= "login.sandbox.mindgard.ai"
 AUTH0_CLIENT_ID= "U0OT7yZLJ4GEyabar11BENeQduu4MaNO"
