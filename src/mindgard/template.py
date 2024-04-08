6ea506680029389952af00f82af47318e3866294
William Hackett <william.hackett@mindgard.ai>
2024-04-08 13:33:02 +0100

    Added simple prompt template validation for custom templates.

--- src/mindgard/template.py
+++ src/mindgard/template.py
@@ -5,8 +5,9 @@ class Template():
         # Load system prompt from file
         self.system_prompt = """You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.\n"""
 
-        # Load system prompt file.
+        # Check provided template syntax contains system_prompt and prompt tags.
         self.prompt_template = "[INST] {system_prompt} {prompt} [/INST]"
+        self.__validate_prompt_template(self.prompt_template)
 
     def __call__(self, prompt):
         #Applies the template to the given prompt
@@ -14,3 +15,16 @@ class Template():
             system_prompt=self.system_prompt,
             prompt=prompt
         )
+    
+    def __validate_prompt_template(self, template):
+        # Validate if the custom template works
+        system_prompt_check = self.__validate_var("{system_prompt}", template)
+        assert system_prompt_check, "{system_prompt} tag is missing from provided template."
+
+        prompt_check = self.__validate_var("{prompt}", template)
+        assert prompt_check, "{prompt} tag is missing from provided template."
+
+    def __validate_var(self, var, template):
+        if(var in template):
+            return True
+        return False
