3fe605c048d230895b9666503644592fa7c47da4
admindgard <alex.dutton@mindgard.ai>
2024-04-12 12:21:14 +0100

    Release 0.18.0

--- src/mindgard/constants.py
+++ src/mindgard/constants.py
@@ -1,5 +1,5 @@
 
-VERSION: str = "0.17.1"
+VERSION: str = "0.18.0"
 REPOSITORY_URL: str = f"https://pypi.org/pypi/mindgard/json"
 AUTH0_DOMAIN= "login.sandbox.mindgard.ai"
 AUTH0_CLIENT_ID= "U0OT7yZLJ4GEyabar11BENeQduu4MaNO"
