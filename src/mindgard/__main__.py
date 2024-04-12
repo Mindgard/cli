01de5dd84db14101b8fe1ee94cb3584d38887032
benji <benji@mindgard.ai>
2024-04-12 08:23:13 +0100

    fix typo

--- src/mindgard/__main__.py
+++ src/mindgard/__main__.py
@@ -58,7 +58,7 @@ def parse_args(args: List[str]) -> argparse.Namespace:
     test_parser.add_argument('--preset', type=str, help='The preset to use', choices=['huggingface', 'openai', 'anthropic', 'custom_mistral'], required=False)
     test_parser.add_argument('--api_key', type=str, help='Specify the API key for the wrapper', required=False)
     test_parser.add_argument('--url', type=str, help='Specify the url for the wrapper', required=False)
-    test_parser.add_argument('--model_name', type=str, help='Specify which model to run againist (OpenAI and Anthropic)', required=False)
+    test_parser.add_argument('--model_name', type=str, help='Specify which model to run against (OpenAI and Anthropic)', required=False)
     test_parser.add_argument('--prompt', type=str, help='Specify the prompt to use', required=False)
     test_parser.add_argument('--system_prompt', type=str, help='Text file containing system prompt to use.', required=False)
     test_parser.add_argument('--selector', type=str, help='The selector to retrieve the text response from the LLM response JSON.', required=False)
