41226ddb1169346b0753184c4be123300dc2fc6a
Stefan Trawicki <stefan.trawicki@mindgard.ai>
2024-06-07 12:33:08 +0100

    let us point CLI at other api gateways
    Co-authored-by: admindgard <admindgard@users.noreply.github.com>

--- src/mindgard/constants.py
+++ src/mindgard/constants.py
@@ -1,8 +1,11 @@
+import os
 
 VERSION: str = "0.33.0"
 REPOSITORY_URL: str = f"https://pypi.org/pypi/mindgard/json"
-AUTH0_DOMAIN= "login.sandbox.mindgard.ai"
-AUTH0_CLIENT_ID= "U0OT7yZLJ4GEyabar11BENeQduu4MaNO"
-AUTH0_AUDIENCE="https://marketplace-orchestrator.com"
+
 ALGORITHMS = ['RS256']
-API_BASE = "https://api.sandbox.mindgard.ai/api/v1"
+AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN", "login.sandbox.mindgard.ai")
+AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID", "U0OT7yZLJ4GEyabar11BENeQduu4MaNO")
+AUTH0_AUDIENCE = os.getenv("AUTH0_AUDIENCE", "https://marketplace-orchestrator.com")
+
+API_BASE = os.getenv("API_BASE", "https://api.sandbox.mindgard.ai/api/v1")
