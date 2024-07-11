760de22e1e1f970e6556d0a19b92035b565f5cbc
stefan <stefan.trawicki@mindgard.ai>
2024-07-11 09:37:59 +0100

    return infrastructure context-specific results table url (#48)

--- src/mindgard/constants.py
+++ src/mindgard/constants.py
@@ -9,6 +9,7 @@ AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID", "U0OT7yZLJ4GEyabar11BENeQduu4MaNO
 AUTH0_AUDIENCE = os.getenv("AUTH0_AUDIENCE", "https://marketplace-orchestrator.com")
 
 API_BASE = os.getenv("API_BASE", "https://api.sandbox.mindgard.ai/api/v1")
+RESULTS_URL = os.getenv("RESULTS_URL", "https://sandbox.mindgard.ai/r/test")
 
 API_RETRY_ATTEMPTS = 10
 API_RETRY_WAIT_BETWEEN_ATTEMPTS_SECONDS = 3
