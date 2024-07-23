89ecee3e352b988b9ff935489c2077d3d57db885
Stefan Trawicki <stefan.trawicki@mindgard.ai>
2024-07-23 15:38:58 +0100

    moved common ui elements to ui_prefabs

--- src/mindgard/run_functions/llm_model_test.py
+++ src/mindgard/run_functions/llm_model_test.py
@@ -1,15 +1,14 @@
 from ..run_poll_display import (
     type_submit_func,
-    type_polling_func,
     type_output_func,
     type_ui_task_map,
     type_ui_exception_map,
     ExceptionCountTuple,
 )
 
-from typing import Optional, Callable, Literal, List, Optional, Dict, Type
+from ..ui_prefabs import poll_and_display_test, output_test_table
 
-from ..utils import print_to_stderr_as_json
+from typing import Optional, Callable, Literal, List, Optional, Dict, Type
 
 from ..orchestrator import (
     setup_orchestrator_webpubsub_request,
@@ -23,8 +22,6 @@ from rich.progress import Progress
 from ..constants import DASHBOARD_URL
 
 import time
-
-import json
 
 import logging
 
@@ -223,34 +220,6 @@ def llm_test_submit_factory(
     return llm_test_submit
 
 
-def poll_and_display_test(
-    access_token: str,
-    ui_task_map: type_ui_task_map,
-    ui_task_progress: Progress,
-    initial_test: OrchestratorTestResponse,
-) -> Optional[OrchestratorTestResponse]:
-    test = get_test_by_id(access_token=access_token, test_id=initial_test.id)
-
-    if len(ui_task_map.keys()) == 0:
-        for attack in test.attacks:
-            ui_task_map[attack.id] = ui_task_progress.add_task(
-                f"Attack {attack.attack}", total=1, status="[chartreuse1]queued"
-            )
-
-    for attack in test.attacks:
-        task_id = ui_task_map[attack.id]
-        if attack.state == 2:
-            ui_task_progress.update(task_id, completed=1, status="[chartreuse3]success")
-        elif attack.state == -1:
-            ui_task_progress.update(task_id, completed=1, status="[red3]failed")
-        elif attack.state == 1:
-            ui_task_progress.update(task_id, status="[orange3]running")
-
-    if test.hasFinished is False:
-        return None
-    return test
-
-
 def llm_test_polling(
     access_token: str,
     initial_test: OrchestratorTestResponse,
@@ -263,35 +232,6 @@ def llm_test_polling(
         ui_task_progress,
         initial_test,
     )
-
-
-def output_test_table(
-    json_out: bool,
-    test: OrchestratorTestResponse,
-    risk_threshold: int,
-) -> Optional[Table]:
-    if json_out:
-        print_to_stderr_as_json(test.model_dump())
-        return None
-    else:
-        table = Table(title=f"Results - {DASHBOARD_URL}/r/test/{test.id}", width=80)
-        table.add_column("Pass", style="cyan")
-        table.add_column("Name", style="magenta")
-        table.add_column("Risk", justify="right", style="green")
-
-        for attack in test.attacks:
-            if attack.state != 2:
-                name = f"Error running '{attack.attack}'"
-                risk_str = "n/a"
-                emoji = "❗️"
-            else:
-                name = attack.attack
-                risk_str = str(attack.risk)
-                emoji = "❌‍" if attack.risk > risk_threshold else "✅️"
-
-            table.add_row(emoji, name, risk_str)
-
-        return table
 
 
 def llm_test_output_factory(risk_threshold: int) -> type_output_func:
