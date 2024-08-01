cc16c2d6374a4de5bb71f211272427aa8662c29b
Stefan Trawicki <stefan.trawicki@mindgard.ai>
2024-08-01 16:30:07 +0100

    completed functionality for alignment calculation

--- src/mindgard/wrappers/image_label_tensor_config.py
+++ src/mindgard/wrappers/image_label_tensor_config.py
@@ -3,9 +3,9 @@ from typing import Dict, List
 from .image import LabelConfidence
 
 
-def image_label_tensor_align(config: Dict[int, str], model_data: List[Dict[str, float]]) -> List[LabelConfidence]:
+def image_label_tensor_align(config: Dict[int, str], model_data: List[LabelConfidence]) -> List[LabelConfidence]:
     if len(config) < len(model_data):
-        raise ValueError("config contained less classes than the model returned!")
+        raise ValueError("Model returned more classes than the config!")
 
     aligned: List[LabelConfidence] = []
 
@@ -13,10 +13,11 @@ def image_label_tensor_align(config: Dict[int, str], model_data: List[Dict[str, 
     for true_index, true_label in config.items():
         aligned.append(LabelConfidence(label=true_label, score=0.0))
 
-    # where we have predictions,
-    for index, label_and_score in enumerate(model_data):
+    # where we have predictions, replace the score with the actual score
+    for index, label_confidence in enumerate(model_data):
         for item in aligned:
-            if item.label == label_and_score.get("label"):
-                item.score = label_and_score.get("score")
+            if item.label == label_confidence.label:
+                item.score = label_confidence.score
+                break
 
     return aligned
