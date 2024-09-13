524cfa7e09c36a781ee2cda5c285ab865362d1fa
Ayomide Apantaku <apantakuayo@gmail.com>
2024-09-13 15:57:25 +0100

    extend cli to support image tests

--- mindgard/main_lib.py
+++ mindgard/main_lib.py
@@ -4,33 +4,51 @@ new library system.
 """
 
 from threading import Thread
-from typing import Dict
+from typing import Any, Dict, Union
 from mindgard.auth import load_access_token
 from mindgard.constants import API_BASE
-from mindgard.test import LLMModelConfig, Test, TestConfig
+from mindgard.test import ImageModelConfig, LLMModelConfig, Test, TestConfig
 from mindgard.test_ui import TestUI
 from mindgard.utils import print_to_stderr
+from mindgard.wrappers.image import ImageModelWrapper
 from mindgard.wrappers.llm import LLMModelWrapper
 
 
-def run_test(final_args:Dict[str, str], model_wrapper: LLMModelWrapper):
+def run_test(final_args:Dict[str, Any], model_wrapper: Union[LLMModelWrapper, ImageModelWrapper]):
   access_token = load_access_token()
   if not access_token:
       print_to_stderr("\033[1;37mRun `mindgard login`\033[0;0m to authenticate.")
       exit(2)
 
-  test_config = TestConfig(
-      api_base=API_BASE,
-      api_access_token=access_token,
-      target=final_args["target"],
-      attack_source="user",
-      parallelism=int(final_args["parallelism"]),
-      model=LLMModelConfig(
-          wrapper=model_wrapper,
-          system_prompt=final_args["system_prompt"],
-      )
-  )
-
+  if final_args["model_type"] == "llm":
+    test_config = TestConfig(
+        api_base=API_BASE,
+        api_access_token=access_token,
+        target=final_args["target"],
+        attack_source="user",
+        parallelism=int(final_args["parallelism"]),
+        model=LLMModelConfig(
+            wrapper=model_wrapper,
+            system_prompt=final_args["system_prompt"],
+        )
+    )
+  elif final_args["model_type"] == "image":
+    test_config = TestConfig(
+        api_base=API_BASE,
+        api_access_token=access_token,
+        target=final_args["target"],
+        attack_source="user",
+        parallelism=int(final_args["parallelism"]),
+        model=ImageModelConfig(
+            wrapper=model_wrapper,
+            dataset=final_args["dataset"],
+            model_type=final_args["model_type"],
+            labels=final_args["labels"],
+        )
+    )
+  else:
+    raise NotImplementedError("Invalid model type") # This should be unreachable due to argparse validations
+  
   test = Test(test_config)
   test_ui = TestUI(test)
 
@@ -39,4 +57,4 @@ def run_test(final_args:Dict[str, str], model_wrapper: LLMModelWrapper):
 
   test_ui.run()
   test_thread.join()
-  exit(1)
+  exit(0)
