3d96c6d91939978e65319459c65062f26289dedf
Imran Bohoran <imran.bohoran@mindgard.ai>
2025-07-04 13:24:01 +0100

    Pulling events from cli for guardrail detection
    * This is the enable guardrail detection on target systems

--- tests/unit/test_recon_guardrail.py
+++ tests/unit/test_recon_guardrail.py
@@ -1,8 +1,10 @@
 import json
 
+import pytest
 import requests_mock
+from pydantic_core._pydantic_core import ValidationError, SchemaError
 
-from mindgard.recon.guardrail import GuardrailService, StartReconRequest
+from mindgard.recon.guardrail import GuardrailService, StartReconRequest, GetEventsRequest, EventResponse
 
 
 class TestReconGuardrailServiceStartRecon:
@@ -12,7 +14,7 @@ class TestReconGuardrailServiceStartRecon:
         mock_return_id = "new-recon-id"
         user_sub = "user"
 
-        guardrail_service = GuardrailService(url=mock_url)
+        guardrail_service = GuardrailService(reconn_url=mock_url, get_events_url=mock_url)
 
         start_recon_request = StartReconRequest(target_name=target_name,
                                                 user_sub=user_sub)
@@ -30,3 +32,47 @@ class TestReconGuardrailServiceStartRecon:
             assert mock_post.request_history[0].headers["x-mg-user"] == user_sub
 
             assert response.id == mock_return_id
+
+
+    def test_get_reconn_events_should_get_selected_events(self):
+        events_url = "http://events.url"
+        reconn_id = "new-recon-id"
+        prompt_request = {
+            "prompt": "hello",
+            "sequence": 1,
+            "language": "en",
+            "is_malicious": False
+        }
+
+        expected_response = {
+            "event_id": "new-prompt-request-event-id",
+            "event_type": "prompt_request",
+            "state": "RUNNABLE",
+            "reconn_id": reconn_id,
+            "prompt_request": [prompt_request]
+        }
+
+        event_request = GetEventsRequest(reconn_id=reconn_id, types=["prompt_request", "complete"])
+
+        guardrail_service = GuardrailService(reconn_url=events_url, get_events_url=events_url)
+
+        with requests_mock.Mocker() as m:
+            mock_events = m.post(events_url, status_code=200, json=json.dumps(expected_response))
+            events_response = guardrail_service.get_recon_events(event_request)
+
+            assert mock_events.call_count == 1
+            assert mock_events.request_history[0].method == "POST"
+            assert mock_events.request_history[0].text == json.dumps(event_request.model_dump_json())
+            assert EventResponse.model_validate(expected_response) == events_response
+
+    def test_get_reconn_events_should_raise_error_for_invalid_event_types(self):
+        events_url = "http://events.url"
+        reconn_id = "new-recon-id"
+
+        with pytest.raises(Exception) as exc_info:
+
+            event_request = GetEventsRequest(reconn_id=reconn_id, types=["prompt_result"])
+            guardrail_service = GuardrailService(reconn_url=events_url, get_events_url=events_url)
+            guardrail_service.get_recon_events(event_request)
+
+        assert isinstance(exc_info.value, ValidationError)
