999859d15811de43a51196444f9a3f612e0f58e2
Ayomide Apantaku <apantakuayo@gmail.com>
2025-07-04 17:39:29 +0100

    Added post to push guardrail detection prompt result
    * This is the enable guardrail detection on target systems

--- mindgard/recon/guardrail.py
+++ mindgard/recon/guardrail.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Literal
+from typing import Literal, Optional
 
 from pydantic import BaseModel
 
@@ -9,7 +9,6 @@ import requests
 @dataclass
 class StartReconRequest:
     target_name: str
-    user_sub: str
 
 
 @dataclass
@@ -27,27 +26,46 @@ class PromptRequest(BaseModel):
     language: str
     is_malicious: bool
 
-class EventResponse(BaseModel):
+class PromptResult(BaseModel):
+    content: str
+    duration_ms: float
+    prompt_request: PromptRequest
+
+class ReceivedEvent(BaseModel):
     event_id: str
     event_type: str
-    state: str
     reconn_id: str
-    prompt_request: list[PromptRequest]
+    prompt_request: Optional[list[PromptRequest]] = None
+
+class PushPromptRequest(BaseModel):
+    reconn_id: str
+    event_type: Literal['prompt_result']
+    prompt_response: list[PromptResult]
+
+class PushPromptResultsResponse(BaseModel):
+    event_id: str
+    message: str
 
 class GuardrailService:
-    def __init__(self, reconn_url: str, get_events_url: str):
+    def __init__(self, reconn_url: str, get_events_url: str, push_events_url: str):
         self.reconn_url = reconn_url
         self.get_events_url = get_events_url
+        self.push_events_url = push_events_url
 
     def start_recon(self, request: StartReconRequest) -> StartReconResponse:
         response = requests.post(self.reconn_url,
-                                 json={"target_name": request.target_name},
-                                 headers={"x-mg-user": request.user_sub})
+                                 json={"target_name": request.target_name})
         return StartReconResponse(response.json())
 
-    def get_recon_events(self, request: GetEventsRequest) -> EventResponse:
+    def get_recon_events(self, request: GetEventsRequest) -> ReceivedEvent:
         event = requests.post(self.get_events_url, json=request.model_dump_json())
-        return EventResponse.model_validate_json(event.json())
+        if event.status_code != 200:
+            raise Exception(f"Failed to get events: {event.text}")
+        return ReceivedEvent.model_validate_json(event.json())
 
-    def push_prompt_results(self):
-        ...
+    def push_prompt_results(self, request: PushPromptRequest) -> PushPromptResultsResponse:
+        response = requests.post(self.push_events_url, json=request.model_dump_json())
+        if response.status_code != 201:
+            raise Exception(f"Failed to prompt results: {response.text}")
+
+        return PushPromptResultsResponse.model_validate_json(response.json())
